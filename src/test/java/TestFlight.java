4da55760e4d0d0f1b634170d987c5e3de1671565
RichardT74 <richtrist@gmail.com>
2019-09-08 17:55:45 +0100

    first

---
+++ src/test/java/TestFlight.java
@@ -0,0 +1,19 @@
+import org.junit.Before;
+//import org.junit.Test;
+
+public class TestFlight {
+
+    private Passenger passenger1;
+
+
+
+   @Before
+    public void setUp( Exception) {
+       passenger1 = new Passenger( "Richard", 3);
+   }
+
+
+
+
+//    @Test
+}
